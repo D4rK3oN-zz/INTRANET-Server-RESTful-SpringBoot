7241d83fd6c812245209e01f7d77d5c7ce323a25
Javier Moreno <javier.moreno@babel.es>
2017-05-11 12:46:30 +0200

    create project - spring initializr

---
+++ src/main/java/com/d4rk3on/intranet/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.d4rk3on.intranet;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(IntranetApplication.class);
+	}
+
+}
