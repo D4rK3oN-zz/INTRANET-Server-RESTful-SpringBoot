4f0f7e857ff9c3002ce08d7e197d4088545f8e8f
Javier Moreno <javier.moreno@babel.es>
2017-05-16 15:49:23 +0200

    new LoginController

---
+++ src/main/java/com/d4rk3on/intranet/common/web/LoginController.java
@@ -0,0 +1,15 @@
+package com.d4rk3on.intranet.common.web;
+
+/**
+ * 
+ * @author javier.moreno
+ *
+ */
+public class LoginController extends GenericController {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1822324567280428583L;
+
+}
