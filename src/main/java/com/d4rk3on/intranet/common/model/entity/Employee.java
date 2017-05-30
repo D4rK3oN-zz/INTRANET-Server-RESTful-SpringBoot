b4d2ee85d9a268b4b87611728e448a95043a5b67
Javier Moreno <javier.moreno@babel.es>
2017-05-30 11:02:05 +0200

    format code

--- src/main/java/com/d4rk3on/intranet/common/model/entity/Employee.java
+++ src/main/java/com/d4rk3on/intranet/common/model/entity/Employee.java
@@ -5,7 +5,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.Data;
 
-@Document(collection="employees")
+@Document(collection = "employees")
 @Data
 public class Employee {
 
